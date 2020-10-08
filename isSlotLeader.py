f5d502fbb4efa38ff6cf2b4e4df588ee910bece7
MokhFn <fnmokhtar@outlook.fr>
2020-10-08 02:53:00 +0200

    Update isSlotLeader.py
    added message for unlucky pools

--- isSlotLeader.py
+++ isSlotLeader.py
@@ -102,4 +102,5 @@ for slot in range(firstSlotOfEpoch,epochLength+firstSlotOfEpoch):
         timestamp = datetime.fromtimestamp(slot + 1591566291, tz=local_tz)
         print(timestamp.strftime('%Y-%m-%d %H:%M:%S') + " ==> Leader for slot " +str(slot-firstSlotOfEpoch) + ", Cumulative epoch blocks: " + str(slotcount))
 
-
+if slotcount == 0:
+    print("The designated pool doesn't have any blocks for this epoch, good luck in the next epoch !!")
