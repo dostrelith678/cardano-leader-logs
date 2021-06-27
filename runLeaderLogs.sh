f864b39aad14fbbe5473fcdfaee3f1590e2630c1
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-06-27 11:38:22 +0100

    revert child process spwan

--- runLeaderLogs.sh
+++ runLeaderLogs.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 echo "Please note that this relies on having a cloned version of:https://github.com/DamjanOstrelic/cardano-leader-logs."
-echo "Then change the directory where this cloning is done into CARDANO_LLOG_EXEC parameter"
-echo "Also we need to have vrf.skey into the VRF_KEY_FILE parameter"
 
 SLOTLEADER_CONFIG="slotLeaderLogsConfig.json" # path to configuration file
 EPOCH_NONCE=$(cardano-cli query protocol-state --mainnet | jq -r .csTickn.ticknStateEpochNonce.contents)
