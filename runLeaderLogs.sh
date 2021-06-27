5d6c6d7d7386c4e18dc1f1909de4150839b4b0d9
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-06-27 11:17:32 +0100

    rename config files

--- runLeaderLogs.sh
+++ runLeaderLogs.sh
@@ -4,7 +4,7 @@ echo "Please note that this relies on having a cloned version of:https://github.
 echo "Then change the directory where this cloning is done into CARDANO_LLOG_EXEC parameter"
 echo "Also we need to have vrf.skey into the VRF_KEY_FILE parameter"
 
-SLOTLEADER_CONFIG="slotLeaderConfig.json" # path to configuration file
+SLOTLEADER_CONFIG="slotLeaderLogsConfig.json" # path to configuration file
 EPOCH_NONCE=$(cardano-cli query protocol-state --mainnet | jq -r .csTickn.ticknStateEpochNonce.contents)
 
 node cardanoLeaderLogs.js $SLOTLEADER_CONFIG $EPOCH_NONCE
