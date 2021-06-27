223ce8b46785c3eb358a15fee1d22d72224437a6
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-06-27 12:24:52 +0100

    doc updates

--- runLeaderLogs.sh
+++ runLeaderLogs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "Please note that this relies on having a cloned version of:https://github.com/DamjanOstrelic/cardano-leader-logs."
-
 SLOTLEADER_CONFIG="slotLeaderLogsConfig.json" # path to configuration file
 EPOCH_NONCE=$(cardano-cli query protocol-state --mainnet | jq -r .csTickn.ticknStateEpochNonce.contents)
 
